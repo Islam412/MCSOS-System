a8129b4981acd2ba37a06f477d55cbe4ee3182d5
Islam412 <islam.124.hamdy@gmail.com>
2026-05-16 02:25:39 +0300

    desing dark mode party

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: {
+          100: '#1e1e2e',
+          200: '#181825',
+          300: '#11111b',
+        }
+      }
+    },
   },
   plugins: [],
 }
