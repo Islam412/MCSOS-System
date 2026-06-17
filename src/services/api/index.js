8231282e0fc3b8e33dd5e9af8038663296ec9acd
Islam412 <islam.124.hamdy@gmail.com>
2026-06-17 21:37:02 +0300

    desing user service api

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,12 +1,12 @@
-// تصدير الخدمات
 export { authService } from './services/authService'
+export { usersService } from './services/usersService'
 export { doctorsService } from './services/doctorsService'
 export { patientsService } from './services/patientsService'
 export { appointmentsService } from './services/appointmentsService'
 export { invoicesService } from './services/invoicesService'
 
-// تصدير التكوين
+
 export { API_CONFIG, ENDPOINTS, ERROR_MESSAGES } from './config'
 
-// تصدير دوال العميل
+
 export { get, post, put, patch, del, uploadFile, setToken, setUser, apiRequest } from './client'
