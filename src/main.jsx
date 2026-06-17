97bde129dcc38c70083a7a3ff644d024a28cb123
Islam412 <islam.124.hamdy@gmail.com>
2026-06-17 20:43:49 +0300

    connect main api

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,33 @@
+// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
-import './i18n'
 import { ThemeProvider } from './context/ThemeContext'
-
-const link = document.createElement('link')
-link.href = 'https://fonts.googleapis.com/css2?family=Cairo:wght@400;500;600;700;800&display=swap'
-link.rel = 'stylesheet'
-document.head.appendChild(link)
+import { ServiceProvider } from './context/ServiceContext' 
+import App from './App'
+import './index.css'
+import './i18n'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider>  {/* ← لف الـ App بـ ThemeProvider */}
-      <App />
-      <Toaster 
-        position="top-right"
-        toastOptions={{
-          duration: 4000,
-          style: {
-            fontFamily: "'Cairo', sans-serif",
-          },
-        }}
-      />
-    </ThemeProvider>
-  </React.StrictMode>,
+    <BrowserRouter>
+      <ThemeProvider>
+        <ServiceProvider>
+          <App />
+          <Toaster 
+            position="top-center"
+            toastOptions={{
+              duration: 3000,
+              style: {
+                background: '#1f2937',
+                color: '#fff',
+                borderRadius: '12px',
+              },
+            }}
+          />
+        </ServiceProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 )
